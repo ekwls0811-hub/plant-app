045f57fdca4fb8820743ab5d3bb2971bdae231ec
ekwls0811-hub <ekwls0811@gmail.com>
2026-04-23 14:03:19 +0900

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,13 +1,17 @@
-messaging.getToken({
-  vapidKey: "BC8eHHRZnarrqnvRJoMyxK5eG3ZlqoZKR56xpIzelt544b8dIBHK0nllgFhSacjjfY7JcyfwVcn6J927oz_d9_Y"
-})
-.then((currentToken) => {
-  if (currentToken) {
-    alert("토큰: " + currentToken);
-  } else {
-    alert("토큰 없음");
-  }
-})
-.catch((err) => {
-  alert("에러: " + err);
+navigator.serviceWorker.register('/plant-app/firebase-messaging-sw.js')
+.then((registration) => {
+  messaging.getToken({
+    vapidKey: "BC8eHHRZnarrqnvRJoMyxK5eG3ZlqoZKR56xpIzelt544b8dIBHK0nllgFhSacjjfY7JcyfwVcn6J927oz_d9_Y",
+    serviceWorkerRegistration: registration
+  })
+  .then((currentToken) => {
+    if (currentToken) {
+      alert("토큰: " + currentToken);
+    } else {
+      alert("토큰 없음");
+    }
+  })
+  .catch((err) => {
+    alert("에러: " + err);
+  });
 });
