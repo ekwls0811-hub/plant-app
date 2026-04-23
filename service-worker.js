35c2c8684e0454d3aa41882c7e041d1c41942c2d
ekwls0811-hub <ekwls0811@gmail.com>
2026-04-23 13:52:00 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,11 @@
+// service-worker.js
 self.addEventListener("install", e => {
   e.waitUntil(
-    caches.open("plant-app-v1").then(cache => {
-      // 상대 경로(./)를 사용하여 현재 폴더 기준으로 파일을 찾도록 합니다.
+    caches.open("plant-app").then(cache => {
       return cache.addAll([
-        "./", 
-        "./index.htm", // index.html이 아니라 index.htm인지 꼭 확인!
-        "./manifest.json"
-      ]).catch(err => console.error("캐시 실패 항목이 있습니다:", err));
+        "./",
+        "./index.html" //
+      ]);
     })
   );
 });
