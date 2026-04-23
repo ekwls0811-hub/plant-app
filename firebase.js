d444887286e6129b7f6777bd169d868f262a0205
ekwls0811-hub <ekwls0811@gmail.com>
2026-04-23 13:56:58 +0900

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,17 +1,13 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyAwnmV_8lt3aZawvDxXDUNnpftJCc4uz0o",
-  authDomain: "plant-app-513f4.firebaseapp.com",
-  projectId: "plant-app-513f4",
-  storageBucket: "plant-app-513f4.firebasestorage.app",
-  messagingSenderId: "966110748803",
-  appId: "1:966110748803:web:ddc825ff986c3b0e003bb6"
-};
-
-firebase.initializeApp(firebaseConfig);
-const messaging = firebase.messaging();
-
-messaging.onMessage(payload => {
-  new Notification(payload.notification.title, {
-    body: payload.notification.body
-  });
+messaging.getToken({
+  vapidKey: "BC8eHHRZnarrqnvRJoMyxK5eG3ZlqoZKR56xpIzelt544b8dIBHK0nllgFhSacjjfY7JcyfwVcn6J927oz_d9_Y"
+})
+.then((currentToken) => {
+  if (currentToken) {
+    alert("토큰: " + currentToken);
+  } else {
+    alert("토큰 없음");
+  }
+})
+.catch((err) => {
+  alert("에러: " + err);
 });
