2e349cabb23b35cdf03992af16aec14755a9e4ca
ekwls0811-hub <ekwls0811@gmail.com>
2026-04-23 13:44:28 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,8 +2,8 @@ self.addEventListener("install", e => {
   e.waitUntil(
     caches.open("plant-app").then(cache => {
       return cache.addAll([
-        "/plant-app/",
-        "/plant-app/index.html",
+        "/",
+        "/index.html"
       ]);
     })
   );
